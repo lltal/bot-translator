b8b5202216bd48050c7f5f4d2fe8070f4ebe2e28
lltal <litvinov_0104@mail.ru>
2024-08-15 22:34:02 +0300

    поправил сборку

--- bot-artifacts/bot-core/src/main/java/com/github/lltal/botcopier/core/BotCoreApplication.java
+++ bot-artifacts/bot-core/src/main/java/com/github/lltal/botcopier/core/BotCoreApplication.java
@@ -1,11 +1,16 @@
 package com.github.lltal.botcopier.core;
 
+import com.github.lltal.botcopier.core.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Import({
+        BotConfig.class
+})
 public class BotCoreApplication {
 
     public static void main(String[] args) {
